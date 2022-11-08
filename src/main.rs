31f0b49aa0a2b1b9033edaebd57daa5710d05606
Antonio Hickey <antoniohickey99@gmail.com>
2022-11-08 13:09:53 -0500

    feat: hello world kernel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-#![no_std] // don't link the Rust standard library (libc)
+#![no_std] // don't link the Rust standard library (no libc)
 #![no_main] // disable Rust entry points
 
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -9,6 +11,21 @@ pub extern "C" fn _start() -> ! {
         The entry point, the linker looks for a function
         named `_start` by default.
     */
+
+    // Cast integer into raw pointer
+    let vga_buff = 0xb8000 as *mut u8;
+
+    // Iterate over the bytes in `HELLO` byte string
+    for (idx, &byte) in HELLO.iter().enumerate() {
+        /* compiler cant prove the raw pointers are valid
+        so put the operations in an unsafe block. */
+        unsafe {
+            // write the string byte and the corresponding color byte (0xb = light cyan)
+            *vga_buff.offset(idx as isize * 2) = byte;
+            *vga_buff.offset(idx as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
